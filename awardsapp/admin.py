cd82d6b90ac5232922b01b0e4335f811a712732f
ngenohzephenia <ngenohzef@gmail.com>
2021-04-06 10:05:59 +0300

    added class methods in test.py

--- awardsapp/admin.py
+++ awardsapp/admin.py
@@ -1,3 +1,9 @@
+
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Profile, Projects
 
 # Register your models here.
+
+admin.site.register(Projects)
+admin.site.register(Profile)
